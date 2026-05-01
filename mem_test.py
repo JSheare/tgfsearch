36184a0849d969c62a9b3776af5106b3c0fc4a31
Jacob Shearer <jacob.shearer6912@gmail.com>
2026-05-01 13:14:37 -0700

    Added new files resulting from refactoring

---
+++ mem_test.py
@@ -0,0 +1,163 @@
+"""A script that measures the average bytes of Detector memory added for every byte of data file imported."""
+import gc as gc
+import numpy as np
+import os as os
+import pandas as pd
+import sys as sys
+
+# Adds grandparent directory to sys.path. Necessary to make the imports below work when running this file as a script
+if __name__ == '__main__':
+    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+
+import tgfsearch.tools as tl
+from tgfsearch.search import search_check, get_detector
+
+def get_obj_size(obj, visited=None):
+    if isinstance(obj, pd.DataFrame):
+        size = obj.memory_usage(deep=True).sum()
+    elif isinstance(obj, np.ndarray):
+        size = obj.nbytes
+    else:
+        size = sys.getsizeof(obj)
+
+    if visited is None:
+        visited = set()
+
+    obj_id = id(obj)
+    if obj_id in visited:
+        return 0
+
+    visited.add(obj_id)
+    if isinstance(obj, pd.DataFrame):
+        pass
+    elif isinstance(obj, dict):
+        for key in obj.keys():
+            size += get_obj_size(key, visited)
+
+        for value in obj.values():
+            size += get_obj_size(value, visited)
+    elif hasattr(obj, '__dict__'):
+        # Iterate through all the object's data members
+        size += get_obj_size(obj.__dict__, visited)
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        for item in obj:
+            size += get_obj_size(item, visited)
+
+    return size
+
+
+def main():
+    if len(sys.argv) >= 4:
+        first_date = str(sys.argv[1])
+        second_date = str(sys.argv[2])
+        unit = str(sys.argv[3])
+    else:
+        print('Error: please provide a first date, a second date, and a unit name.')
+        exit()
+
+    if len(sys.argv) > 4:
+        mode_info = sys.argv[4:]
+    else:
+        mode_info = []
+
+    # Flag for a custom import directory
+    if len(mode_info) >= 2 and '-c' in mode_info:
+        import_loc = mode_info[mode_info.index('-c') + 1]
+    else:
+        import_loc = ''
+
+    # Flag for ignoring scintillators
+    if len(mode_info) >= 2 and '-i' in mode_info:
+        index = mode_info.index('-i')
+        if index < len(mode_info) - 1:
+            end_index = index + 1
+            for i in range(index + 1, len(mode_info)):
+                if '-' in mode_info[i]:
+                    break
+
+                end_index += 1
+
+            ignore_scints = mode_info[index + 1:end_index]
+        else:
+            ignore_scints = []
+
+    else:
+        ignore_scints = []
+
+    # Makes sure inputs are valid
+    feedback = search_check(first_date, second_date, unit)
+    print(feedback[1])
+    if not feedback[0]:
+        exit()
+
+    lm_growth_factors = []
+    trace_growth_factors = []
+    try:
+        for date_str in tl.make_date_list(first_date, second_date):
+            print(f'Importing data for {date_str}...')
+            detector = get_detector(unit, date_str)
+            if import_loc != '':
+                try:
+                    detector.set_import_loc(import_loc)
+                    if not detector.has_identity():
+                        raise FileNotFoundError("couldn't infer identity.")
+
+                except FileNotFoundError:
+                    print('Error: no data files to infer detector identity from. Please provide the import location of '
+                          'the data.')
+                    exit()
+
+            import_scints = []
+            for scintillator in detector:
+                if scintillator not in ignore_scints:
+                    import_scints.append(scintillator)
+
+            # Measuring the growth factor for the list mode files
+            detector.import_data(import_traces=False, import_scints=import_scints, feedback=True)
+            fileset_size = 0
+            for scintillator in detector:
+                lm_filelist = detector.get_attribute(scintillator, 'lm_filelist', deepcopy=False)
+                for file in lm_filelist:
+                    fileset_size += tl.file_size(file)
+
+            if fileset_size > 0:
+                lm_growth_factors.append(get_obj_size(detector) / fileset_size)
+
+            detector.clear()
+            gc.collect()
+
+            # Measuring the growth factor for the trace files
+            detector.import_data(import_lm=False, import_scints=import_scints, feedback=True)
+            fileset_size = 0
+            for scintillator in detector:
+                trace_filelist = detector.get_attribute(scintillator, 'trace_filelist', deepcopy=False)
+                for file in trace_filelist:
+                    fileset_size += tl.file_size(file)
+
+            if fileset_size > 0:
+                trace_growth_factors.append(get_obj_size(detector) / fileset_size)
+
+            detector.clear()
+            gc.collect()
+            print('\n')
+
+        # Printing the final average growth factors
+        if len(lm_growth_factors) > 0:
+            print(f'Average list mode data growth factor: {sum(lm_growth_factors) / len(lm_growth_factors)}.')
+        else:
+            print('No list mode data growth factors to average.')
+
+        if len(trace_growth_factors) > 0:
+            print(f'Average trace data growth factor: {sum(trace_growth_factors) / len(trace_growth_factors)}.')
+        else:
+            print('no trace data growth factors to average.')
+
+        print('\n')
+
+    except MemoryError:
+        print('Error: not enough memory available on system to complete the test.')
+        exit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tgfsearch/helpers/helper_funcs.py
@@ -0,0 +1,187 @@
+"""A module containing functions used by various parts of the package."""
+import os
+import struct
+
+import tgfsearch.parameters as params
+
+
+def make_path(path):
+    """Checks to see if a directory path corresponding to the given string exists and, if not, creates it.
+
+    Parameters
+    ----------
+    path : str
+        The path to be created.
+
+    """
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def file_size(file, uncompressed=True):
+    """Returns the size of the given file in bytes.
+
+    Parameters
+    ----------
+    file : str
+        The name of the file.
+    uncompressed : bool
+        Optional. If True, the function will return the uncompressed file size (if the file is compressed). True
+        by default. Note: this will not be accurate for files that are over 4GB uncompressed due to the way that
+        uncompressed size is stored according to the gzip standard.
+
+    Returns
+    -------
+    int
+        The size of the file in bytes.
+
+    """
+
+    if uncompressed and len(file) > 3 and file[-3:] == '.gz':
+        with open(file, 'rb') as f:
+            f.seek(-4, 2)
+            size = struct.unpack('I', f.read(4))[0]
+
+    else:
+        size = os.path.getsize(file)
+
+    return size
+
+
+def days_per_month(month, year):
+    """Returns the number of days in the requested month based on the year.
+
+    Parameters
+    ----------
+    month : int
+        The month of the year (1-12).
+    year : int
+        The desired year to check.
+
+    Returns
+    -------
+    int
+        The number of days in the specified month for the specified year.
+
+    """
+
+    match month:
+        case 1:  # January
+            return 31
+        case 2:  # February
+            return 29 if year % 4 == 0 or (year % 100 != 0 and year % 400 == 0) else 28
+        case 3:  # March
+            return 31
+        case 4:  # April
+            return 30
+        case 5:  # May
+            return 31
+        case 6:  # June
+            return 30
+        case 7:  # July
+            return 31
+        case 8:  # August
+            return 31
+        case 9:  # September
+            return 30
+        case 10:  # October
+            return 31
+        case 11:  # November
+            return 30
+        case 12:  # December
+            return 31
+        case _:
+            return 0
+
+
+def roll_date_forward(date_str):
+    """Returns the calendar date after the one given as an argument.
+
+    Parameters
+    ----------
+    date_str : str
+        The date to be rolled forward from (in yymmdd format).
+
+    Returns
+    -------
+    str
+        The calendar date after the one supplied (in yymmdd format).
+
+    """
+
+    date_int = int(date_str)
+    date_int += 1
+    date_str = str(date_int)
+    # Month rollover
+    if int(date_str[4:]) > days_per_month(int(date_str[2:4]), int(params.CENTURY + date_str[0:2])):
+        date_int = date_int + 100 - (int(date_str[4:]) - 1)
+        date_str = str(date_int)
+
+    # Year rollover
+    if int(date_str[2:4]) > 12:
+        date_int = (date_int // 10000 + 1) * 10000 + 101
+        date_str = str(date_int)
+
+    return date_str
+
+
+def roll_date_backward(date_str):
+    """Returns the calendar date before the one given as an argument.
+
+    Parameters
+    ----------
+    date_str : str
+        The date to be rolled backward from (in yymmdd format).
+
+    Returns
+    -------
+    str
+        The calendar date before the one supplied (in yymmdd format).
+
+    """
+
+    date_int = int(date_str)
+    date_int -= 1
+    date_str = str(date_int)
+    # Year rollback
+    if int(date_str[2:]) == 100:  # This would be January 0th because int(0100) = 100
+        date_int = (date_int // 10000 - 1) * 10000 + 1231  # December 31st of the previous year
+        date_str = str(date_int)
+
+    # Month rollback
+    if int(date_str[4:]) == 0:
+        date_int -= 100
+        date_int += days_per_month(int(str(date_int)[2:4]), int(params.CENTURY + date_str[0:2]))
+        date_str = str(date_int)
+
+    return date_str
+
+
+def make_date_list(first_date, second_date):
+    """Makes a list of dates from first_date to second_date (inclusive).
+
+    Parameters
+    ----------
+    first_date : str
+        The first date in the range.
+    second_date : str
+        The second date in the range.
+
+    Returns
+    -------
+    list
+        A list of dates on the specified range.
+
+    """
+
+    requested_dates = [first_date]
+    if first_date != second_date:
+        date_str = first_date
+        while True:
+            date_str = roll_date_forward(date_str)
+            requested_dates.append(date_str)
+            if date_str == second_date:
+                break
+
+    return requested_dates
